490426a93a7a802ee252fc146290e7291d0dd136
SirTomax <46323295+SirTomax@users.noreply.github.com>
2019-01-05 17:14:22 +0100

    Add project files.

---
+++ sort_integers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sort_integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            // Variables
+            int currentNumber = 0;
+            int oldNumber = 0;
+            int iter = 0;
+
+            Console.WriteLine("Enter a maximum of 15 integers in rising order (end with 0):");
+
+            do
+            {
+                // Get input from user
+                currentNumber = Convert.ToInt32(Console.ReadKey());
+
+                if (iter < 15 && 0 < currentNumber && currentNumber < 100)
+                {
+
+                }
+
+            } while (currentNumber != 0);
+
+        }
+    }
+}

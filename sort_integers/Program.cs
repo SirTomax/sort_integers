a3b47c038d1b438805b352df0214830a0b5c5d3e
SirTomax <46323295+SirTomax@users.noreply.github.com>
2019-01-06 10:21:29 +0100

    Done - But it doesn't do anything interesting

--- sort_integers/Program.cs
+++ sort_integers/Program.cs
@@ -15,20 +15,32 @@ namespace sort_integers
             int oldNumber = 0;
             int iter = 0;
 
-            Console.WriteLine("Enter a maximum of 15 integers in rising order (end with 0):");
+            Console.WriteLine("Enter a maximum of 15 integers between 0 and 100 in rising order (end with 0):");
 
             do
             {
                 // Get input from user
-                currentNumber = Convert.ToInt32(Console.ReadKey());
+                currentNumber = Convert.ToInt32(Console.ReadLine());
 
                 if (iter < 15 && 0 < currentNumber && currentNumber < 100)
                 {
-
+                    if (currentNumber > oldNumber)
+                    {
+                        iter++;
+                        var numberString = Convert.ToString(currentNumber);
+                        Console.WriteLine("OK > " + numberString);
+                        oldNumber = currentNumber;
+                    }
+                    else
+                        Console.WriteLine("Number needs to be larger than the last one!");
+                }
+                else if (currentNumber > 99)
+                {
+                    Console.WriteLine("Number can't be lager than 99!");
                 }
 
             } while (currentNumber != 0);
-
+            Console.WriteLine("Done!");
         }
     }
 }
